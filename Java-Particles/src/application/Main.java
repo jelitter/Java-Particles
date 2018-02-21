fe75a7bc03a862ce77d50ecf72c342a3d1d92ae6
Isaac Sanchez <jelito@gmail.com>
2018-02-21 16:24:44 +0000

    Particles changed

--- Java-Particles/src/application/Main.java
+++ Java-Particles/src/application/Main.java
@@ -4,20 +4,22 @@ import java.util.ArrayList;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.geometry.Point2D;
 import javafx.stage.Stage;
-import model.Particle;
+//import model.Particle;
+import model.Vehicle;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 
 
 public class Main extends Application {
 	
-	private static int NUM_PARTICLES = 2;
+	private static int NUM_PARTICLES = 1;
 	public static final double WIDTH = 1300;
 	public static final double HEIGHT = 900;
 
-	private ArrayList<Particle> particles;
-	private Particle target;
+	private ArrayList<Vehicle> vehicles;
+	private Point2D target;
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -28,30 +30,27 @@ public class Main extends Application {
 			Scene scene = new Scene(root, WIDTH, HEIGHT);
 			
 
-			particles = new ArrayList<Particle>();
+			vehicles = new ArrayList<Vehicle>();
 			
 			for (int i = 0; i < NUM_PARTICLES; i++) {
-				Particle p = new Particle();
-				p.localToParent(root.getLayoutBounds());
-				particles.add(p);
-//				root.getChildren().add(p);
+				vehicles.add(new Vehicle());
 			}
 			
-			root.getChildren().addAll(particles);
+			root.getChildren().addAll(vehicles);
 			
 			primaryStage.setTitle("Particle test");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			
 			root.setOnMouseClicked(e -> {
-				target = new Particle(e.getX(), e.getY());
+				target = new Point2D(e.getX(), e.getY());
 				System.out.println("New target:" + e.getX() + ", " + e.getY());
 				animate();
 			});
 			
 			double x = WIDTH/2;
 			double y = HEIGHT/2;
-			target = new Particle(x,y);
+			target = new Point2D(x,y); // Initial target
 			animate();
 		
 		} catch(Exception e) {
@@ -67,14 +66,18 @@ public class Main extends Application {
 		new AnimationTimer() {
 	    	@Override
 	    	public void handle(long now) {
-	    		for (Particle p: particles) {
-//	    			p.rotate();
-	    			p.seek(target.pos);
-	    			p.print();
-//	    			target.print();
-	    			p.edges();
-	    			p.update();
-//	    			p.update2();
+	    		for (Vehicle v: vehicles) {
+	    			v.seek(target);
+	    			v.update();
+	    			v.display();
+	    			v.print();
+	    			if (v.getPos().distance(target) < 5) {
+	    				double x = Math.floor(Math.random() * WIDTH);
+	    				double y = Math.floor(Math.random() * HEIGHT);;
+	    				
+	    				target = new Point2D(x, y);
+	    				animate();
+	    			}
 	    		}
 	    	}
 	    }.start();
